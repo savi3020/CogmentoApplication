ec726b1b60a0b1492be2f7f02b1677256d076ab8
savi3020 <165770472+savi3020@users.noreply.github.com>
2024-07-15 23:11:16 +0530

    BDD FremeWork is created

---
+++ src/main/java/Github/com/BDDFrameWork_github/App.java
@@ -0,0 +1,13 @@
+package Github.com.BDDFrameWork_github;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
